58378d547c5f18d783084c72dc91bc6138f71e46
Abdelkader Madoui <abdelkadermadoui@gmail.com>
2021-01-17 14:40:21 +0100

    Implementing a search function

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+
+        // Watch out for the number 3, as the number of cmd args will change later.
+        if args.len() < 3 {
+            return Err("Not enough arguments")
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)?;
+
+    for line in search(&config.query, &contents) {
+        println!("Search results =====> {}", line);
+    }
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);        
+        }
+    }
+
+    results
+}
--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,28 @@
-use std::process;
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::prelude::*;
-use std::env;
+extern crate minigrep;
 
+use std::process;
+use std::env;
+use minigrep::Config;
 
 fn main() {
     let args : Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem while parsing arguments: {}", err);
+
         process::exit(1);
     });
     
-    println!("Searching for: {}", config.query);
-    println!("Searching in: {}", config.filename);
     // println!("Contents in: {}", read_file(&config.filename));
 
-    run(config);
+    println!("Searching for: {}", config.query);
+    println!("Searching in: {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
     
 }
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-
-        if args.len() < 3 {
-            return Err("Not enough arguments")
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn run(config: Config) {
-    let mut f = File::open(config.filemame).expect("File not found");
-
-    let mut contents = String::new();
-
-    f.read_to_string(&mut contents)
-        .expect("Couldn't read file");
-
-    println!("Contents =======>> \n{}", contents);
-}
